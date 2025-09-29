430a4279e797861ab6415f038168d8ba9f48d91c
Javier Nicolas Martinez <javierformosanm@gmail.com>
2025-09-29 11:17:41 -0300

    Controlador de asset casi terminado

--- src/controllers/assets.controller.js
+++ src/controllers/assets.controller.js
@@ -1,6 +1,31 @@
+import { AssetModel } from "../models/mongoose/asset.model.js";
+
 export const createAsset = async (req, res) => {
+  const {
+    inventoryNumber,
+    description,
+    brand,
+    model,
+    status,
+    acquisitionDate,
+    acquisitionValue,
+    responsible,
+    categories,
+  } = req.body;
   try {
     // TODO: crear asset (usuario autenticado)
+    const asset = await AssetModel.create({
+      inventoryNumber,
+      description,
+      brand,
+      model,
+      status,
+      acquisitionDate,
+      acquisitionValue,
+      responsible,
+      categories,
+    });
+
     return res.status(201).json({ msg: "Asset creado correctamente" });
   } catch (error) {
     console.log(error);
@@ -11,6 +36,7 @@ export const createAsset = async (req, res) => {
 export const getAllAssets = async (_req, res) => {
   try {
     // TODO: listar assets con el responsible y sus categories (populate) (solo admin)
+    const assets = await AssetModel.find();
     return res.status(200).json({ data: assets });
   } catch (error) {
     console.log(error);
@@ -19,8 +45,10 @@ export const getAllAssets = async (_req, res) => {
 };
 
 export const getMyAssets = async (req, res) => {
+  const logeado = req.my - assets;
   try {
     // TODO: assets con sus categories (populate) del usuario logueado (solo si el usuario logueado es responsible de assets)
+
     return res.status(200).json({ data: myAssets });
   } catch (error) {
     return res.status(500).json({ msg: "Error interno del servidor" });
@@ -28,8 +56,10 @@ export const getMyAssets = async (req, res) => {
 };
 
 export const deleteAsset = async (req, res) => {
+  const { id } = req.params;
   try {
     // TODO: eliminar un asset (solo si el usuario logueado es el responsible del asset)
+    const deletedAsset = await AssetModel.findByIdAndDelete(id);
     return res.status(204).json({ msg: "Asset eliminado correctamente" });
   } catch (error) {
     return res.status(500).json({ msg: "Error interno del servidor" });
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,27 @@
+import { comparePassword, hashPassword } from "../helpers/bcrypt.helper.js";
+import { signToken } from "../helpers/jwt.helper.js";
+import { UserModel } from "../models/mongoose/user.model.js";
+
 export const register = async (req, res) => {
+  const { username, email, password, role, profile } = req.body;
+  const hashedPassword = await hashPassword(password);
   try {
     // TODO: crear usuario con password hasheada y profile embebido
-    return res.status(201).json({ msg: "Usuario registrado correctamente" });
+    const newUser = await UserModel.create({
+      username,
+      email,
+      password: hashedPassword,
+      role,
+      profile: {
+        employee_number,
+        first_name,
+        last_name,
+        phone,
+      },
+    });
+    return res
+      .status(201)
+      .json({ msg: "Usuario registrado correctamente", newUser });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ msg: "Error interno del servidor" });
@@ -9,8 +29,33 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const { username, password } = req.body;
   try {
     // TODO: buscar user, validar password, firmar JWT y setear cookie httpOnly
+    const user = await UserModel.findOne({
+      username,
+    });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ ok: false, message: "Credenciales invalidas" });
+    }
+    const validarPassword = await comparePassword(password, user.password);
+    if (!validarPassword) {
+      return res
+        .status(401)
+        .json({ ok: false, message: "credenciales invalidas" });
+    }
+    const token = signToken({
+      _id: user._id,
+      name: user.profile.first_name,
+      lastname: user.profile.last_name,
+      role: user.role,
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60, // 1 hora
+    });
     return res.status(200).json({ msg: "Usuario logueado correctamente" });
   } catch (error) {
     console.log(error);
@@ -21,6 +66,15 @@ export const login = async (req, res) => {
 export const getProfile = async (req, res) => {
   try {
     // TODO: devolver profile del user logueado actualmente
+    const data = req.user;
+
+    const profile = await UserModel.findById(data._id);
+    // .select("profile username _id");
+    if (!profile) {
+      return res
+        .status(404)
+        .json({ ok: false, message: "Usuario no encontrado" });
+    }
     return res.status(200).json({ data: profile });
   } catch (error) {
     console.log(error);
@@ -29,6 +83,16 @@ export const getProfile = async (req, res) => {
 };
 
 export const logout = async (_req, res) => {
-  res.clearCookie("token");
-  return res.status(204).json({ msg: "Sesión cerrada correctamente" });
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({ msg: "Sesión cerrada correctamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ ok: false, message: "Error interno del servidor" });
+  }
 };
+
+// POST /api/auth/register: Registro con creación automática de profile (público)
+// ● POST /api/auth/login: Login con JWT en cookie (público)
+// ● GET /api/auth/profile: Obtener profile del usuario autenticado (usuario autenticado)
+// ● POST /api/auth/logout: Logout limpiando cookie (usuario autenticado)
